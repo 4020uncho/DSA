875cce1e3d803a08e9fba69ddf34b1377aa29d08
4020uncho <uncholimbu5@gmail.com>
2025-12-01 13:35:05 +0545

    Repo changed prev uploads

---
+++ src/Basics/datatypes.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include <string>
+using namespace std;
+int main(){
+    // //int
+    // int x=10;
+    // //long
+    // long x=15;
+    // cin>>x;
+    // cout<<x;
+    // return 0;
+    //float
+    // float x=10.5;
+    // cout<<x;    
+    // return 0;
+    //string and getline
+    // string s1;
+    // string s2;
+    // cin>>s1;
+    // cin>>s2; // it will take input only till space
+    // cout<<s1;
+    // cout<<s2;
+    // return 0; 
+    // string str;
+    // getline(cin,str); 
+    // cout<<str;
+    // return 0;
+    // it will take input including space till new line
+    //char
+    char ch='a';
+    cout<<ch;
+    return 0;
+}
