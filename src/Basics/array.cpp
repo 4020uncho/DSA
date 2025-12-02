c994a7f76c721d122022b5bbf3978312e77821a3
4020uncho <uncholimbu5@gmail.com>
2025-12-02 12:26:40 +0545

    basics of array

---
+++ src/Basics/array.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int arr[5];
+    // cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+    // cout << arr[1];
+    // return 0; // 1d array
+
+    //2D array
+    int arr[3][4]; // 3 rows and 4 columns
+    //taking input
+    arr[1][3] = 1;
+    cout << arr[1][3];
+    return 0;
+
+}
