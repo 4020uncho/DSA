aa99ab02d7810ae0d32baa55375e3035e3e14fbe
4020uncho <uncholimbu5@gmail.com>
2025-12-03 10:57:03 +0545

    while loop logic

---
+++ src/Basics/whileloop.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int i = 1; // initialization
+    while(i<=5) { // condition
+        cout << "Hello, World!" << endl;
+        i++; // updation
+    }
+    return 0;
+}
