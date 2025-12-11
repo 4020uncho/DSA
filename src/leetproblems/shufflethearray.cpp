e383678c9c11a2744d4bf38eff320dded0206c44
4020uncho <uncholimbu5@gmail.com>
2025-12-11 06:22:40 +0545

    shuffling of the array solved

---
+++ src/leetproblems/shufflethearray.cpp
@@ -0,0 +1,13 @@
+//the only logic of the code is present in the function shuffle
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> res;
+        int x=nums.size();
+        for(int i=0;i<x-n;i++){
+            res.push_back(nums[i]);
+            res.push_back(nums[i+n]);
+        }
+        return res;
+    }
+};
