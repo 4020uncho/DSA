5c7f38bbc996aa34cc7e0dd8cee23467c04f973b
4020uncho <uncholimbu5@gmail.com>
2025-12-23 08:48:44 +0545

    Feat: Solved monotonic stack concept question daily temperature and learned the basic logic with it too

---
+++ src/leetproblems/Monotonic_stack_02.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> answer(n, 0);
+        stack<int> st;  
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
+                int prevDay = st.top();
+                st.pop();
+                answer[prevDay] = i - prevDay;
+            }
+            st.push(i);
+        }
+        return answer;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> temperatures = {73, 74, 75, 71, 69, 72, 76, 73}; //preloaded main function
+
+    vector<int> result = sol.dailyTemperatures(temperatures);
+
+    cout << "Output: ";
+    for (int x : result) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
